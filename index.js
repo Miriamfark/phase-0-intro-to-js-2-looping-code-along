1e08ddfb26505076a412c32a4a8485bade04e41d
Miriam Farkas <miriamjoshua00@gmail.com>
2021-12-24 00:01:19 +0200

    Completed Assign

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+const names = []
+
+function writeCards(names, event) {
+    for (let i = 0; i < names.length; i++) {
+        names.push(`Thank you ${names[i]} for your beautiful ${event} gift!`)
+    }
+return names
+}
+
+let countDown = 15
+while (countDown > 0) {
+    console.log(countDown--)
+}
